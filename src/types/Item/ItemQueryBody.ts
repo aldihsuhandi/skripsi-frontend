314cc21203f702f4f35f5ed09d55a485481b3b4a
Chandra Wijaya <chandrawinterwar@gmail.com>
2023-03-31 13:02:16 +0700

    make itemFilterContext optional, kata Aldih

--- src/types/Item/ItemQueryBody.ts
+++ src/types/Item/ItemQueryBody.ts
@@ -2,7 +2,7 @@ import { ItemFilterContext } from "./ItemFilterContext";
 
 // Gw liat semua Request item body ini sama
 export interface ItemQueryBody {
-  itemFilterContext: ItemFilterContext;
+  itemFilterContext?: ItemFilterContext;
   pageNumber?: number;
   numberOfItem?: number;
 }
