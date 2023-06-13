f7c7cb0909e8c1f373e09f977b19b0cb8fe11d32
Chandra Wijaya <chandrawinterwar@gmail.com>
2023-06-13 23:26:00 +0700

    change BE_URL ke yang beneran, nggak localhost

--- src/types/Client.ts
+++ src/types/Client.ts
@@ -1,9 +1,9 @@
 export const CLIENT_ID = "dbf0201e-23a0-446b-8db3-40b1b6ed7c1f";
 export const CLIENT_SECRET = "dycvervbrngjwhryugwduo";
 export const Session_Local_Key = "Shumi-sessionKey";
-// export const BE_URL =
-//   "https://shumishumi-be-dot-moonlit-helper-388513.et.r.appspot.com";
-export const BE_URL = "http://localhost:8080";
+export const BE_URL =
+  "https://shumishumi-be-dot-moonlit-helper-388513.et.r.appspot.com";
+// export const BE_URL = "http://localhost:8080";
 
 // export const CLIENT_ID = "dbf0201e-23a0-446b-8db3-40b1b6ed7c1f";
 // export const CLIENT_SECRET = "dycvervbrngjwhryugwduo";
